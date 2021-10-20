25ad92e99b1de9c0fd88623887e22b7ab7fdf5a0
Cardos Vasile <cardosvasile15@gmail.com>
2021-10-20 10:17:14 +0200

    test lab 4

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    lst = []
+    while True:
+        print('1. Citirea listei de floaturi fiecare numar fiind separat prin spatiu: ')
+        print('2. Citirea listei de floaturi fiecare numar fiind separat prin spatiu: ')
+        print('3. Citirea listei de floaturi fiecare numar fiind separat prin spatiu: ')
+        print('4. Citirea listei de floaturi fiecare numar fiind separat prin spatiu: ')
+        print('5. Citirea listei de floaturi fiecare numar fiind separat prin spatiu: ')
+        print('x. Citirea listei de floaturi fiecare numar fiind separat prin spatiu: ')
+
