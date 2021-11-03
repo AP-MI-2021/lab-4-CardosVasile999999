6299b2abfcfbba3b366b3a4a32044f05ec79f5c4
Cardos Vasile <cardosvasile15@gmail.com>
2021-11-03 15:54:58 +0100

    lab 4

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def read_list():
 
 
 def get_integers_part(lst):
-    '''
+    """"
     Cu aceasta functie vom returna partea intreaga a tuturor numerelor din lista
     :param lst: lista de float-uri
     :return: lista de intregi
-    '''
+    """
     result = []
     for element in lst:
         result.append(int(element))
@@ -29,16 +29,16 @@ def test_get_integers_part():
 
 
 def get_all_numbers_in_open_interval(lst, s, d):
-    '''
+    """
     Functie care are ca paramtrii o lista si capetele intervalului si care returneaza toate numerele din interval
     :param lst: lista de float-uri
     :param s: capatul din stanga
     :param d: capatul din dreapta
     :return: toate elementele care fac parte din interval
-    '''
+    """
     result = []
     for element in lst:
-        if element > s and element < d:
+        if d > element > s:
             result.append(element)
     return result
 
@@ -47,76 +47,78 @@ def test_get_all_numbers_in_open_interval():
     assert get_all_numbers_in_open_interval([1, 2, 3, 4, 5], 2, 5) == [3, 4]
     assert get_all_numbers_in_open_interval([], 12, 34) == []
     assert get_all_numbers_in_open_interval([-1.4, -56, 34.5, 65, 23.54, 45.01], 700, 800) == []
-    assert get_all_numbers_in_open_interval([1.5, -3.3, 8, 9.8, 3.2], -4, 5) ==  [1.5, -3.3, 3.2]
+    assert get_all_numbers_in_open_interval([1.5, -3.3, 8, 9.8, 3.2], -4, 5) == [1.5, -3.3, 3.2]
     assert get_all_numbers_in_open_interval([1.4, 6, 7, 9], 5, 10) == [6, 7, 9]
 
 
 def get_all_elements_with_integer_part_being_a_divisor_of_fractional_part(lst):
-    '''
-    Functie ce are ca parametrii lista de float-uri si returneaza elementele care au partea intreaga divizor al partii fractionale
+    """
+    Functie ce are ca parametrii lista de float-uri si returneaza elementele care au partea intreaga divizor
+    al partii fractionale
     :param lst: lista de floaturi
     :return: o lista cu elementele ce au partea intreaga divizor al partii fractionale
-    '''
+    """
     result = []
     for element in lst:
         string_element = str(element)
-        decimale = string_element.split('.')[1]
-        intreg = string_element.split('.')[0]
-        fract = int(decimale)
-        it = int(intreg)
-        if fract % it == 0 and fract != 0:
-            result.append(element)
+        if int(element) != element:
+            decimale = string_element.split('.')[1]
+            intreg = string_element.split('.')[0]
+            fract = int(decimale)
+            it = int(intreg)
+            if fract % it == 0 and fract != 0:
+                result.append(element)
     return result
 
 
 def test_get_all_elements_with_integer_part_being_a_divisor_of_fractional_part():
     assert get_all_elements_with_integer_part_being_a_divisor_of_fractional_part([1.5, 2.6]) == [1.5, 2.6]
-    assert get_all_elements_with_integer_part_being_a_divisor_of_fractional_part([]) == []
+    assert get_all_elements_with_integer_part_being_a_divisor_of_fractional_part([6]) == []
+    assert get_all_elements_with_integer_part_being_a_divisor_of_fractional_part([2.7, 8, 2, 4]) == []
 
 
 def get_characther_into_word(lst):
-    '''
+    """
     functie care transforma fiecare caracter intrun cuvant
     :param lst: lisat de flaoturi
     :return: lista de caractere
-    '''
+    """
     result = []
     for element in lst:
-        aux = ' '
+        aux = ''
         elementu = str(element)
+
         for j in elementu:
             if j == '-':
-                minus = "minus"
-                aux = aux + minus
+                aux = aux + 'minus'
             elif j == '1':
-                unu = 'unu'
-                aux = aux + unu
+                aux = aux + 'unu'
             elif j == '2':
-                doi = 'doi'
-                aux = aux +doi
+                aux = aux + 'doi'
             elif j == '3':
-                trei = 'trei'
-                aux = aux +trei
+                aux = aux + 'trei'
             elif j == '4':
-                patru = 'patru'
-                aux = aux +patru
+                aux = aux + 'patru'
             elif j == '5':
-                cinci = 'cinci'
-                aux = aux +cinci
+                aux = aux + 'cinci'
             elif j == '6':
                 aux = aux + 'sase'
             elif j == '7':
-                sapte = 'sapte'
-                aux = aux +sapte
+                aux = aux + 'sapte'
             elif j == '8':
-                opt =  'opt'
-                aux = aux +opt
+                aux = aux + 'opt'
             elif j == '9':
-                aux = aux +'noua'
-            elif j == '.':
-                aux = aux +'virgula'
+                aux = aux + 'noua'
+            elif j == '.' and int(element) != element:
+                aux = aux + 'virgula'
         result.append(aux)
     return result
+
+
+def test_get_characther_into_word():
+    assert get_characther_into_word([]) == []
+    assert get_characther_into_word([-123, 1.3, 8]) == ['minusunudoitrei', 'unuvirgulatrei', 'opt']
+    assert get_characther_into_word([-5.69]) == ['minuscincivirgulasasenoua']
 
 
 def main():
@@ -155,4 +157,5 @@ if __name__ == '__main__':
     test_get_integers_part()
     test_get_all_numbers_in_open_interval()
     test_get_all_elements_with_integer_part_being_a_divisor_of_fractional_part()
+    test_get_characther_into_word()
     main()
